f7ebfea9246d31082d724f3c11be0d85c5020088
Michael Schumann <schumann82@gmail.com>
2018-08-05 13:22:57 -0700

    folder structure and initial code

---
+++ config/orm.js
@@ -0,0 +1,60 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+
+
+// Object for all our SQL statement functions.
+const orm = {
+  selectAll: function(table, cb) {
+    let queryString = "SELECT * FROM " + table + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function(table, cols, vals, cb) {
+    let queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  
+  updateOne: function(table, condition, cb) {
+    let queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += true;
+    queryString += " WHERE ";
+    
+    queryString += condition;
+    
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
+};
+
+// Export the orm object for the model (burgers.js).
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Swiss Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rodeo Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Angus Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Apocalypse Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bbq Burger', false);
